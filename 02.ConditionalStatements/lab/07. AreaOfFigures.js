0990cd6a913642c79685001a6b08b8ac480bea03
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-01-10 18:27:40 +0200

    Add files via upload

---
+++ 02.ConditionalStatements/lab/07. AreaOfFigures.js
@@ -0,0 +1,25 @@
+function areaOfFigure(input) {
+
+    let type = input[0];
+    let a = Number(input[1]);
+    let area = 0;
+
+    if(type == "square"){
+        area = a * a;
+    }else if(type == "rectangle"){
+        let b = Number(input[2]);
+        area = a * b;
+    }else if(type == "circle"){
+        area = Math.PI * Math.pow(a,2);
+    }else{
+        let h = Number(input[2]);
+        area = a * h / 2;
+    }
+
+    console.log(area.toFixed(3));
+}
+
+areaOfFigure(["triangle",
+"4.5",
+"20"])
+;
