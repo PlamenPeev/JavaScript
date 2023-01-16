ea29e351b2c167489f53d237244c17263f1f0767
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-01-16 19:35:32 +0200

    Add files via upload

---
+++ 03. ConditionalStatementsAdvanced/lab/07.WorkingHours.js
@@ -0,0 +1,14 @@
+function workingHour(input) {
+let hour = Number(input[0]);
+let day = input[1];
+
+if ( (day == "Monday" || day == "Tuesday" || day == "Wednesday"  || day == "Thursday" || day == "Friday" || day == "Saturday")
+&& (hour >= 10 && hour <=18)){
+console.log("open");
+}else{
+console.log("closed");
+}
+}
+
+workingHour(["11",
+"Monday"]);
