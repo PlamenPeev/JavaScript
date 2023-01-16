f432d0411d7c6cb3e7b6c55b8e24c877a0b2f7aa
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-01-16 19:06:05 +0200

    Add files via upload

---
+++ 03. ConditionalStatementsAdvanced/lab/06. NumberInRange.js
@@ -0,0 +1,11 @@
+function checkNumber(input) {
+let num = Number(input[0]);
+
+if( num >= -100 && num <= 100 && num != 0){
+    console.log("Yes");
+}else{
+    console.log("No");
+}
+}
+
+checkNumber(["-25"]);
