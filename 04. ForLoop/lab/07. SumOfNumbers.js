557419761073629b20e3fb035598a98c3ce80b32
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-02-04 20:10:07 +0200

    Add files via upload

---
+++ 04. ForLoop/lab/07. SumOfNumbers.js
@@ -0,0 +1,10 @@
+function sumOfNumbers(input) {
+    let text = input[0];
+    let sum = 0;
+    for( let i = 0; i < text.length; i++ ){
+        let num = Number(text[i]);
+        sum += num;
+    }
+    console.log(`The sum of the digits is:${sum}`);
+    }
+    sumOfNumbers(["564891"]);
