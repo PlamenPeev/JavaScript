9f08fd0ce4e4ef560f52478389a5b667d22959c3
PlamenPeev <37194180+PlamenPeev@users.noreply.github.com>
2023-03-24 22:17:11 +0200

    Add files via upload

---
+++ 06.NestedLoops/Lab/06. Building.js
@@ -0,0 +1,25 @@
+function building(input) {
+let floors = Number(input[0]);
+let rooms = Number(input[1]);
+
+for(let i = floors; i>= 1; i--){
+    let printLine = "";
+    for(let j = 0; j< rooms; j++){
+
+        if(i == floors){
+            printLine += `L${i}${j} `;
+        }else{
+            if(i % 2 == 0){
+                printLine += `O${i}${j} `;
+            }else{
+                printLine += `A${i}${j} `;
+            }
+        }
+    }
+    console.log(printLine);
+}
+
+
+}
+
+building(["9", "5"]);
